1a64d75958e32ad076e14914e8d7d2f17c9c0a3b
Jon Betti <jonbetti@gmail.com>
2018-04-20 09:50:13 -0400

    Emit FileDescriptorSets during protoc

--- proto/update.sh
+++ proto/update.sh
@@ -2,7 +2,9 @@
 # Generates go code for a set of directories' protobufs.
 
 function generate() {
-  protoc --go_out=plugins=grpc:. $(find "$1" |grep -E "\.proto$")
+  protoc "--descriptor_set_out=$1_descriptors.protobin" \
+    --go_out=plugins=grpc:. \
+    $(find "$1" |grep -E "\.proto$")
 }
 
 cd $(dirname $(realpath "$0"))
